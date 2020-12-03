f22dce50ecc68b171ee09b5055695bac57570af9
tikuwa <tikuwamk2@gmail.com>
2020-12-03 16:45:39 +0900

    顔認識以外のアドレスを追加しました

--- img_list.py
+++ img_list.py
@@ -14,5 +14,35 @@ def image_list():
     return render_template("img_list.html", image_type=image_type, image_paths=image_paths)
 
 
+@app.route('/grayscale', methods=["GET"])
+def image_list():
+    base_dir = 'output_grayscale'
+    app.static_folder = base_dir
+    image_paths = os.listdir("./"+base_dir+"/")
+    image_type = 'Grayscale'
+
+    return render_template("img_list.html", image_type=image_type, image_paths=image_paths)
+
+
+@app.route('/canny', methods=["GET"])
+def image_list():
+    base_dir = 'output_canny'
+    app.static_folder = base_dir
+    image_paths = os.listdir("./"+base_dir+"/")
+    image_type = 'Canny'
+
+    return render_template("img_list.html", image_type=image_type, image_paths=image_paths)
+
+
+@app.route('/face', methods=["GET"])
+def image_list():
+    base_dir = 'input'
+    app.static_folder = base_dir
+    image_paths = os.listdir("./"+base_dir+"/")
+    image_type = 'Face'
+
+    return render_template("img_list.html", image_type=image_type, image_paths=image_paths)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
